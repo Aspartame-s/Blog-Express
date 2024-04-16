fd8483c2213769c63574af4b7b55cc1c16a81884
Aspartame-s <jiahuicheng639@gmail.com>
2024-04-16 18:32:47 +0800

    blogrouter 代码

--- controller/blogController.js
+++ controller/blogController.js
@@ -1,13 +1,11 @@
-const {exec, escape} = require('../db/mysql')
+const {exec} = require('../db/mysql')
 const xss = require('xss')
 //获取博客列表
 const getList = (author, keyword) => {
     console.log(author)
-    author = escape(author)
-    // keyword = escape(keyword)
     let sql = `select * from blogs where 1=1 `
     if(author) {
-        sql += `and author=${author} `
+        sql += `and author='${author}' `
     }
     if(keyword) {
         sql += `and title like '%${keyword}%' `
@@ -28,11 +26,9 @@ const newBlog = (blogData = {}) => {
     //blogData 是post接口的 body参数
     let {title, content, author} = blogData
     const createTime = Date.now()
-    title = xss(escape(title))
-    console.log(title)
-    content = xss(escape(content))
-    author = escape(author)
-    let sql = `insert into blogs (title, content, createtime, author) values (${title}, ${content}, ${createTime}, ${author})`
+    title = xss(title)
+    content = xss(content)
+    let sql = `insert into blogs (title, content, createtime, author) values ('${title}', '${content}', ${createTime}, '${author}')`
     return exec(sql).then(insertData => {
         return {
             id: insertData.insertId
@@ -43,9 +39,7 @@ const newBlog = (blogData = {}) => {
 const updateBlog = (id, blogData = {}) => {
     // return true
     const {title, content} = blogData
-    title = escape(title)
-    content = escape(content)
-    let sql = `update blogs set title=${title}, content=${content} where id='${id}'`
+    let sql = `update blogs set title='${title}', content='${content}' where id='${id}'`
     return exec(sql).then(updateData => {
         if(updateData.affectedRows > 0) {
             return true
